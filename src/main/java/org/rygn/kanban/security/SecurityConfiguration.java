d840a59339672e2b4e682f185550cf4cb6efbb41
rgirodon <remy.girodon@gmail.com>
2020-08-24 15:17:31 +0200

    Initial commit

---
+++ src/main/java/org/rygn/kanban/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package org.rygn.kanban.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http		
+		.authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/oauth/**").permitAll()
+        .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
+        .anyRequest().authenticated();
+	}
+}
